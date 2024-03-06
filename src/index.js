37602af49b284ce76fcbfbe0a4aa4d74020749ca
Vinicius C Reck <vinicius.reck@nn.nl>
2024-03-06 11:21:08 +0100

    feat: get code ready

--- src/index.js
+++ src/index.js
@@ -8,20 +8,16 @@
 const parcelSortingSystem = (deliveriesForTheDay) => {
   const sorted = deliveriesForTheDay
     .sort((a, b) => a.destination.localeCompare(b.destination))
-    // FIX: invert sorts, first by deliveryAddress then by destination
-    .sort(
-      // FIX: invert sorts. so '1' => '-1' and '-1' => '1', also it could be replaced by localeCompare
-      (a, b) =>
-        a.deliveryAddress < b.deliveryAddress
-          ? 1
-          : a.deliveryAddress > b.deliveryAddress
-          ? -1
-          : 0,
+    .sort((a, b) =>
+      a.deliveryAddress < b.deliveryAddress
+        ? 1
+        : a.deliveryAddress > b.deliveryAddress
+        ? -1
+        : 0,
     );
 
   return [
     ...sorted.filter((delivery) => delivery.sameCity),
-    // FIX: remove '!' or remove '=== false'
     ...sorted.filter((delivery) => !delivery.sameCity === false),
   ];
 };
@@ -33,34 +29,13 @@ const parcelSortingSystem = (deliveriesForTheDay) => {
  * @return {Array} The modified array of deliveries with sameCity marked.
  */
 const markSameCityDeliveries = (deliveries) =>
-  deliveries.map(
-    // FIX: change else 'delivery' => '{ ...delivery, sameCity: false }'
-    (delivery) =>
-      delivery.origin === delivery.destination
-        ? { ...delivery, sameCity: true }
-        : delivery,
+  deliveries.map((delivery) =>
+    delivery.origin === delivery.destination
+      ? { ...delivery, sameCity: true }
+      : delivery,
   );
 
 const orderDeliveries = (deliveries) =>
   parcelSortingSystem(markSameCityDeliveries(deliveries));
-
-/*
-
-In case of struggle with test we can offer a option to print the result 
-
- const deliveriesForTheDay = [
-  { deliveryAddress: '42 Wallaby Way', origin: 'Amsterdam', destination: 'Amsterdam' },
-  { deliveryAddress: '531 Elm St', origin: 'Rotterdam', destination: 'Amsterdam' },
-  { deliveryAddress: '947 Oak St', origin: 'Amsterdam', destination: 'Rotterdam' },
-  { deliveryAddress: '124 Pine St', origin: 'Utrecht', destination: 'Amersfoort' },
-  { deliveryAddress: '622 Maple St', origin: 'Amersfoort', destination: 'Utrecht' },
-  { deliveryAddress: '42 Wallaby Way', origin: 'Rotterdam', destination: 'Amsterdam' },
-  { deliveryAddress: '42 Wallaby Way', origin: 'Utrecht', destination: 'Amsterdam' },
-  { deliveryAddress: '123 Main St', origin: 'Rotterdam', destination: 'Rotterdam' },
-  { deliveryAddress: '123 Main St', origin: 'Amersfoort', destination: 'Rotterdam' },
-];
-
-console.table(orderDeliveries(deliveriesForTheDay)); 
-*/
 
 module.exports = orderDeliveries;
